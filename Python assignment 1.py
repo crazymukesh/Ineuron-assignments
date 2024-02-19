bcf641429c33d9b658f1812e871a5b8b02fbc8e2
MUKESH <66762889+crazymukesh@users.noreply.github.com>
2024-02-19 08:14:01 +0530

    Add files via upload

---
+++ Python assignment 1.py
@@ -0,0 +1,99 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "637b47a5",
+   "metadata": {},
+   "source": [
+    "## Programming Assignment_1\n",
+    "-------------"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "0c647dbe",
+   "metadata": {},
+   "source": [
+    "### 1.print("Hello Python")
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "fc18a6cd",
+   "metadata": {},
+   "source": [
+    "### 2.def perform_operations(a, b):
+    sum = a + b
+    if b != 0:
+        division = a / b
+    else:
+        division = 'Undefined, cannot divide by zero'
+    return sum, division\n"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "af6e2429",
+   "metadata": {},
+   "source": [
+    "### 3.def triangle_area(base, height):
+    return 0.5 * base * height\n"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "b8140061",
+   "metadata": {},
+   "source": [
+    "### 4.# Initialize variables
+a = 5
+b = 10
+
+# Print initial values
+print("Initial values: a =", a, ", b =", b)
+
+# Swap variables
+a, b = b, a
+
+# Print swapped values
+print("Swapped values: a =", a, ", b =", b)
+\n"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "3f8054c1",
+   "metadata": {},
+   "source": [
+    "### 5.import random
+
+# Generate a random number between 0 and 100
+random_number = random.randint(0, 100)
+
+print(f"The random number is {random_number}")
+\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
